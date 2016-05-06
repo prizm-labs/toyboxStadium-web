8c809a9b373afd5ef09be452413366e40b8123b1
Edward Yun <eyun1488@gmail.com>
2016-05-06 13:16:46 -0700

    media queries for mobile

--- app/js/controllers/baseStat.js
+++ app/js/controllers/baseStat.js
@@ -7,9 +7,29 @@ function BaseStatsCtrl($scope,$firebaseObject,$firebaseArray,AppSettings) {
 
 	var list = $firebaseArray(new Firebase(AppSettings.basestatsUrl));
 	vm.baseStatsListing = list;
-	// console.log(list);
 
-	$scope.OneAtATime = true;
+	//// this scope will only allow one accordion tab to be openned at a time. ////
+	// $scope.OneAtATime = true;
+
+	//// function to toggle collapse and close accordion ////
+	// $scope.status = function(){
+	// 	for(var i = 0; i < vm.baseStatsListing.length; i++){
+	// 		vm.baseStatsListing[i].isopen == false;
+	// 		if(vm.baseStatsListing[i].isopen == true){
+	// 			vm.baseStatsListing[i].isopen == false;
+	// 			console.log('get if')
+	// 		} else {
+	// 			vm.baseStatsListing[i].isopen == true;
+	// 			console.log('getting in else');
+	// 		}
+	// 	}
+	// };
+	// $scope.status = {
+	// 	isOpen: new Array($scope.groups.length)
+	// };
+	// for(var i = 0; i < $scope.status.isOpen.length; i++) {
+	// 	$scope.status.isOpen[i] = ( i === 0 );
+	// }
 
 
 }
